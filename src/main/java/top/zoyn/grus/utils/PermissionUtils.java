802bcad1684fef0716ea2617f64cc2a3a594a706
泠辰 <93849805+Crsuh2er0@users.noreply.github.com>
2023-01-18 18:52:57 +0800

    Create PermissionUtils.java

---
+++ src/main/java/top/zoyn/grus/utils/PermissionUtils.java
@@ -0,0 +1,30 @@
+package top.zoyn.grus.utils;
+
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+import top.zoyn.grus.I18N;
+
+/**
+ * @author Crsuh2er0
+ * @apiNote
+ * @since 2023/1/18
+ */
+public class PermissionUtils {
+    private PermissionUtils() {
+    }
+
+    /**
+     * @return true if the sender is not op
+     * false if the sender is op
+     */
+    public static boolean nonAdminAuth(@NotNull CommandSender sender, boolean notify) {
+        if (sender.isOp()) {
+            return false;
+        } else {
+            if(notify){
+                sender.sendMessage(I18N.MESSAGE_PREFIX.getMessage() + I18N.NO_PERMISSION.getMessage());
+            }
+            return true;
+        }
+    }
+}
